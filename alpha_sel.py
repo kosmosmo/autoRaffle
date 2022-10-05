d442bea40209d722b1346a7017335351dbffede8
kosmosmo <kosmosmo@gmail.com>
2022-10-05 03:33:51 -0400

    fixasdf

--- alpha_sel.py
+++ alpha_sel.py
@@ -40,10 +40,11 @@ def _write_cache(data):
         json.dump(data, outfile, indent=4)
 
 class alphaJobs():
-    def __init__(self,url,keyword,rid):
+    def __init__(self,url,keyword,rid,name):
         self.url = url
         self.keyword= keyword
         self.rid = rid
+        self.name = name
         self.driver = self.get_driver()
         time.sleep(2)
         self.driver.maximize_window()
@@ -190,7 +191,7 @@ class alphaJobs():
         return res
 
     def _click_reg(self):
-        if self._check_captcha():
+        if self._check_captcha() and self.name:
             find_rec = at_obj.get("alpha fails", filter_by_formula='FIND("{}", Url)'.format(self.url)).get(
                 'records')
             if not find_rec:
@@ -271,7 +272,7 @@ def run_all_jobs():
             while tried < 3:
                 try:
                     if url not in cache or ignore:
-                        job = alphaJobs(url,keyword,rid)
+                        job = alphaJobs(url,keyword,rid,name)
                         job.run()
                         cache[url] = ""
                         _write_cache(cache)
