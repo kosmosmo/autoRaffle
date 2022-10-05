81630b5aa82d7264cd5a267c7f4242edcdbedc55
kosmosmo <kosmosmo@gmail.com>
2022-10-04 21:53:22 -0400

    fixasdf

--- alpha_sel.py
+++ alpha_sel.py
@@ -316,7 +316,7 @@ def auto_clean_pref():
             pass
         time.sleep(5)
 
-rand_time = random.randint(1, 2)
+rand_time = random.randint(1, 100)
 time.sleep(rand_time)
 delet_bad_pref()
 run_all_jobs()
