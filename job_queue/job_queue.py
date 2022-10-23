ff30aa3db8a967d88970e8c7856489cee581d645
kosmosmo <kosmosmo@gmail.com>
2022-10-23 07:11:35 -0400

    fixasdf

--- job_queue/job_queue.py
+++ job_queue/job_queue.py
@@ -2,7 +2,7 @@ import datetime,os,sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import alpha_sel_q as alpha_runner
 import premint_sel_q as premint_runner
-import twitter_job
+import time
 def convert_to_airtable_time(date_time):
     at_time =  date_time.strftime("%Y-%m-%d %H:%M:%S")
     return at_time
@@ -17,6 +17,7 @@ class job_queue():
     def run(self):
         i = 1
         for item in self.res:
+            self.auto_clean_pref()
             print (str(i) + '/' + str(len(self.res)))
             print (item)
             tried = 0
@@ -27,6 +28,27 @@ class job_queue():
                 except:
                     tried += 1
             i += 1
+
+    def auto_clean_pref(self):
+        bad_file_path = r'C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default\Preferences.bad'
+        pref_file_path = r'C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default\Preferences'
+        if os.path.exists(bad_file_path) and os.path.getsize(bad_file_path) > 100000:
+            os.remove(bad_file_path)
+            print("deleted bad pref!!")
+            time.sleep(5)
+        if os.path.exists(pref_file_path) and os.path.getsize(pref_file_path) > 100000:
+            try:
+                self.clean_pref()
+                print("clean pref!!")
+            except:
+                pass
+            time.sleep(5)
+
+    def clean_pref(self):
+        pref_file_path = r'C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default\Preferences'
+        to_address = "C:\\Users\\Administrator\\Desktop\\autoRaffle-master\\Preferences"
+        import shutil
+        shutil.copyfile(to_address, pref_file_path)
 
 
 class base_job():
