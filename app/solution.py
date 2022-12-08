22c0751596de0b014990c7d25ea1b35b047e8778
kosmosmo <kosmosmo@gmail.com>
2022-12-08 07:21:02 -0500

    f

--- app/solution.py
+++ app/solution.py
@@ -90,7 +90,7 @@ class Solution(object):
         return verify_button
 
     def verify_single_captcha(self, index):
-        time.sleep(10)
+        time.sleep(5)
         elements = self.wait.until(EC.visibility_of_all_elements_located(
             (By.CSS_SELECTOR, '#rc-imageselect-target table td')))
         single_captcha_element: WebElement = elements[index]
