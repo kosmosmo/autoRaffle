7a93ccbad8e833031af7fc57bfb3fc37bedf1523
kosmosmo <kosmosmo@gmail.com>
2022-12-28 21:57:59 -0500

    t

--- happ/solution.py
+++ happ/solution.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 import time
-from loguru import logger
+#from loguru import logger
 from happ.captcha_resolver import CaptchaResolver
 from happ.settings import CAPTCHA_SINGLE_IMAGE_FILE_PATH
 from happ.utils import resize_base64_image
@@ -63,7 +63,7 @@ class Solution(object):
         self.switch_to_captcha_content_iframe()
         captcha_element: WebElement = self.get_captcha_element()
         if captcha_element.is_displayed:
-            logger.debug('trigged captcha successfully')
+            pass
 
     def get_captcha_target_text(self) -> WebElement:
         captcha_target_name_element: WebElement = self.wait.until(EC.presence_of_element_located(
@@ -81,15 +81,15 @@ class Solution(object):
             By.CSS_SELECTOR, '#anchor #checkbox'
         )))
         checked = anchor.get_attribute('aria-checked')
-        logger.debug(f'checked {checked}')
+        #logger.debug(f'checked {checked}')
         return str(checked) == 'true'
 
     def verify_captcha(self):
         # get target text
         self.captcha_target_text = self.get_captcha_target_text()
-        logger.debug(
-            f'captcha_target_text {self.captcha_target_text}'
-        )
+        #logger.debug(
+        #    f'captcha_target_text {self.captcha_target_text}'
+        #)
         # extract all images
         single_captcha_elements = self.wait.until(EC.visibility_of_all_elements_located(
             (By.CSS_SELECTOR, '.task-image .image-wrapper .image')))
@@ -101,8 +101,8 @@ class Solution(object):
             match_result = re.search(pattern, single_captcha_element_style)
             single_captcha_element_url = match_result.group(
                 1) if match_result else None
-            logger.debug(
-                f'single_captcha_element_url {single_captcha_element_url}')
+            #logger.debug(
+            #    f'single_captcha_element_url {single_captcha_element_url}')
             with open(CAPTCHA_SINGLE_IMAGE_FILE_PATH % (str(i),), 'wb') as f:
                 f.write(requests.get(single_captcha_element_url).content)
             resized_single_captcha_base64_string = resize_base64_image(
@@ -119,18 +119,20 @@ class Solution(object):
             self.captcha_target_text
         )
         if not captcha_recognize_result:
-            logger.error('count not get captcha recognize result')
+            pass
+            #logger.error('count not get captcha recognize result')
             return
         recognized_results = captcha_recognize_result.get(
             'solution', {}).get('objects')
 
         if not recognized_results:
-            logger.error('count not get captcha recognized indices')
+            pass
+            #logger.error('count not get captcha recognized indices')
             return
 
         # click captchas
         recognized_indices = [i for i, x in enumerate(recognized_results) if x]
-        logger.debug(f'recognized_indices {recognized_indices}')
+        #logger.debug(f'recognized_indices {recognized_indices}')
         click_targets = self.wait.until(EC.visibility_of_all_elements_located(
             (By.CSS_SELECTOR, '.task-image')))
         for recognized_index in recognized_indices:
@@ -147,7 +149,8 @@ class Solution(object):
         # check if succeed
         is_succeed = self.get_is_successful()
         if is_succeed:
-            logger.debug('verifed successfully')
+            pass
+            #logger.debug('verifed successfully')
         else:
             self.switch_to_captcha_content_iframe()
             self.verify_captcha()
